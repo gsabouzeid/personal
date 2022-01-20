676532758f237cedaeeb6a6f06cdf59f58c0c4b8
Garrett <gsabouzeid@gmail.com>
2022-01-19 18:59:49 -0600

    Added more C algorithms

---
+++ Algorithms/C/BinarySearch.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int start, int end, int target) {
+    while (start <= end) {
+        int mid = (start + end) / 2;
+        if(target == arr[mid]) {
+            return mid;
+        } else if (target < arr[mid]) {
+            end = mid - 1;
+        } else if (target > arr[mid]) {
+            start = mid + 1;
+        }
+    }
+    return -1;
+}
+
+void selectionSort(int arr[], int length) {
+    for (int i = 0; i < length-1; i++) {
+        int min_index = i;
+        for (int j = i+1; j < length; j++) {
+            if (arr[j] < arr[min_index]) {
+                min_index = j;
+            }
+        }
+        int temp = arr[min_index];
+        arr[min_index] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    int arr[] = {61,32,87,45,12,7,4,89,65};
+    int length = sizeof(arr) / sizeof(arr[0]);
+    int start = 0;
+    int end = length - 1;
+    int target = 61;
+
+    printf("Unsorted Array: ");
+    for (int i = 0; i < length; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    selectionSort(arr, length);
+
+    printf("\nSorted Array: ");
+    for (int i = 0; i < length; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    int result = binarySearch(arr, start, end, target);
+    
+    if (result == -1) {
+        printf("\nTarget not present in array");
+    } else {
+        printf("\nTarget found at index: %d", result);
+    }
+
+    return 0;
+}
