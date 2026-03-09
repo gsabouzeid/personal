f09403da10369de9300cee6f5f5e20560b53adc5
Garrett <gsabouzeid@gmail.com>
2026-03-08 23:43:15 -0500

    refactor: add proper speedDial

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -23,6 +23,12 @@ import MenuIcon from "@mui/icons-material/Menu";
 import MailIcon from "@mui/icons-material/Mail";
 import { useTranslation } from "react-i18next";
 import { useColorScheme } from "@mui/material/styles";
+import SpeedDial from "@mui/material/SpeedDial";
+import SpeedDialIcon from "@mui/material/SpeedDialIcon";
+import SpeedDialAction from "@mui/material/SpeedDialAction";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+
+const actions = [{ icon: <PlaylistAddIcon />, name: "Create New List" }];
 
 function App() {
   const { t } = useTranslation();
@@ -111,6 +117,23 @@ function App() {
           <Item key={key} item={item} index={key} setItemList={setItemList} />
         ))}
       </Stack>
+      <SpeedDial
+        ariaLabel="SpeedDial basic example"
+        sx={{ position: "fixed", bottom: 16, right: 16 }}
+        icon={<SpeedDialIcon />}
+      >
+        {actions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            slotProps={{
+              tooltip: {
+                title: action.name,
+              },
+            }}
+          />
+        ))}
+      </SpeedDial>
     </>
   );
 }
