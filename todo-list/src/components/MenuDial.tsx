90db40c9e0860d32f7bbf25a56a11bc0d19f41d8
Garrett <gsabouzeid@gmail.com>
2026-03-14 16:28:57 -0500

    refactor: switch to single control button

--- todo-list/src/components/MenuDial.tsx
+++ todo-list/src/components/MenuDial.tsx
@@ -1,34 +1,20 @@
-import { PlaylistAddCircleOutlined } from "@mui/icons-material";
-import SpeedDial from "@mui/material/SpeedDial";
-import SpeedDialAction from "@mui/material/SpeedDialAction";
-import SpeedDialIcon from "@mui/material/SpeedDialIcon";
+import AddIcon from "@mui/icons-material/Add";
+import { Fab, Tooltip } from "@mui/material";
 import { useTranslation } from "react-i18next";
 
 function MenuDial() {
   const { t } = useTranslation();
 
-  const actions = [
-    { icon: <PlaylistAddCircleOutlined />, name: t("list.createNewList") },
-  ];
-
   return (
-    <SpeedDial
-      ariaLabel={t("list.listControls")}
-      sx={{ position: "fixed", bottom: 16, right: 16 }}
-      icon={<SpeedDialIcon />}
-    >
-      {actions.map((action) => (
-        <SpeedDialAction
-          key={action.name}
-          icon={action.icon}
-          slotProps={{
-            tooltip: {
-              title: action.name,
-            },
-          }}
-        />
-      ))}
-    </SpeedDial>
+    <Tooltip title={t("list.createNewList")} placement="left" arrow>
+      <Fab
+        color="primary"
+        aria-label={t("list.createNewList")}
+        sx={{ position: "fixed", bottom: 16, right: 16 }}
+      >
+        <AddIcon />
+      </Fab>
+    </Tooltip>
   );
 }
 
