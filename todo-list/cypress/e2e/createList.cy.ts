f16f06dfc889fd4f2c86c96668c50173f2aa615e
Garrett <gsabouzeid@gmail.com>
2026-03-17 23:36:54 -0500

    feat: add cypress e2e testing and make a few tests

---
+++ todo-list/cypress/e2e/createList.cy.ts
@@ -0,0 +1,18 @@
+describe("create list spec", () => {
+  it("can create a new list through the menu dial", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get("button[aria-label='Create New List']").click();
+    cy.get("#listName").type("My New List");
+    cy.get("button[type='submit']").click();
+    cy.contains("My New List").should("be.visible");
+  });
+
+  it("can create a new list through side navigation", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get("button[aria-label='Show Lists Toggle']").click();
+    cy.get("div[role='button']").contains("Create New List").click();
+    cy.get("#listName").type("My New List 2");
+    cy.get("button[type='submit']").click();
+    cy.contains("My New List 2").should("be.visible");
+  })
+});
---
+++ todo-list/cypress/e2e/spec.cy.ts
@@ -0,0 +1,5 @@
+describe("login spec", () => {
+  it("can access to app", () => {
+    cy.visit("http://localhost:5173/");
+  });
+});
