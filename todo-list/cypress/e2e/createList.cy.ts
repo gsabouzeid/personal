78cd396dfe958d6835c456ab583c46d81023ee7e
Garrett <gsabouzeid@gmail.com>
2026-03-17 23:50:04 -0500

    refactor: log into site before each test

--- todo-list/cypress/e2e/createList.cy.ts
+++ todo-list/cypress/e2e/createList.cy.ts
@@ -1,6 +1,9 @@
 describe("create list spec", () => {
-  it("can create a new list through the menu dial", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:5173/");
+  });
+
+  it("can create a new list through the menu dial", () => {
     cy.get("button[aria-label='Create New List']").click();
     cy.get("#listName").type("My New List");
     cy.get("button[type='submit']").click();
@@ -8,11 +11,10 @@ describe("create list spec", () => {
   });
 
   it("can create a new list through side navigation", () => {
-    cy.visit("http://localhost:5173/");
     cy.get("button[aria-label='Show Lists Toggle']").click();
     cy.get("div[role='button']").contains("Create New List").click();
     cy.get("#listName").type("My New List 2");
     cy.get("button[type='submit']").click();
     cy.contains("My New List 2").should("be.visible");
-  })
+  });
 });
