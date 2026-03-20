10cdae2e96368ed9eaabaea64579d323b32ee8ac
Garrett <gsabouzeid@gmail.com>
2026-03-19 21:39:50 -0500

    refactor: login spec

--- todo-list/cypress/e2e/login.cy.ts
+++ todo-list/cypress/e2e/login.cy.ts
@@ -1,5 +1,8 @@
 describe("login spec", () => {
   it("can access to app", () => {
     cy.visit("http://localhost:5173/");
+    cy.contains("Feels empty here... Create a new list to get started!").should(
+      "be.visible",
+    );
   });
 });
