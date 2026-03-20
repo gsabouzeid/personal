37769a0ba128c2a39bbc29d5d8517f0c44ccefa8
Garrett <gsabouzeid@gmail.com>
2026-03-19 22:30:46 -0500

    feat:  manage list spec

--- todo-list/cypress/locators/todoListLocators.ts
+++ todo-list/cypress/locators/todoListLocators.ts
@@ -2,9 +2,10 @@ export default {
   createNewListButton: "button[aria-label='Create New List']",
 
   listNameInput: "#list-name",
-  submitButton: "button[type='submit']",
+  submitButton: "button[type='submit']:contains('Create')",
 
   showListsToggle: "button[aria-label='Show Lists Toggle']",
+  listToggle: (listName: string) => `div[role='button']:contains(${listName})`,
   sideNavCreateNewListbutton: "div[role='button']:contains('Create New List')",
   settingsButton: "div[role='button']:contains('Settings')",
 
